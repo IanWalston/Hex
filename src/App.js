e59cdbc1dc6ca88f647b926dd6e5bd841fffcec7
Ian Walston <ikwalston@comcast.net>
2019-03-08 18:56:23 -0500

    added tooltips

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hexagon from "./components/Hexagon";
 import info from "./info.json";
 import projects from "./projects.json";
 import hexlinks from "./hexlinks.json";
+import ReactTooltip from "react-tooltip";
 
 class App extends React.Component {
   state = { info, projects, hexlinks, hexlinkson: true, darktheme: false };
@@ -12,6 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <Wrapper darktheme={this.state.darktheme}>
+        <ReactTooltip />
         <div class="row" id="header">
           <div className="col-md-8">
             <div className="row">
@@ -26,13 +28,14 @@ class App extends React.Component {
               <p>{this.state.info.description}</p>
             </div>
           </div>
-          <div className="col-md-3" style={{"min-height":"300px"}}>
+          <div className="col-md-3" style={{ "min-height": "300px" }}>
             {this.state.hexlinkson ? (
               <div>
                 <ul id="projects">
                   {this.state.hexlinks.map(link => {
                     return (
                       <Hexagon
+                        tooltip={link.tooltip}
                         image={link.hexicon}
                         link={link.link}
                         darktheme={this.state.darktheme}
@@ -50,6 +53,7 @@ class App extends React.Component {
           <div className="col-md-1">
             <div className="row mt-3">
               <img
+                data-tip="list view"
                 alt="hex view controller"
                 width="32px"
                 height="32px"
@@ -57,6 +61,11 @@ class App extends React.Component {
                   this.state.hexlinkson
                     ? "./img/icons/list.png"
                     : "./img/icons/hexagon.png"
+                }
+                data-tip={
+                  this.state.hexlinkson
+                    ? "view links as list"
+                    : "view links as hexagon"
                 }
                 onClick={() =>
                   this.setState({ hexlinkson: !this.state.hexlinkson })
@@ -67,7 +76,7 @@ class App extends React.Component {
               <input
                 type="button"
                 value="Dark"
-                style={{backgroundColor:"black",color:"white"}}
+                style={{ backgroundColor: "black", color: "white" }}
                 className=""
                 onClick={() =>
                   this.setState({ darktheme: !this.state.darktheme })
--- src/components/Hexagon/index.js
+++ src/components/Hexagon/index.js
@@ -4,10 +4,19 @@ import "./style.css";
 function Hexagon(props) {
   return (
     <li>
-      <div onClick={()=>{window.location = props.link}}>
-          <img className={props.darktheme?`inverted`:null}  src={props.image} alt="" />
-          <h1>{props.name}</h1>
-          <p>{props.description}</p>
+      <div
+        data-tip={props.tooltip}
+        onClick={() => {
+          window.location = props.link;
+        }}
+      >
+        <img
+          className={props.darktheme ? `inverted` : null}
+          src={props.image}
+          alt=""
+        />
+        <h1>{props.name}</h1>
+        <p>{props.description}</p>
       </div>
     </li>
   );
