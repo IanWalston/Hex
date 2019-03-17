4f5e937a5024a4e58793e6cea1474df9183c26d2
Ian Walston <ikwalston@comcast.net>
2019-03-17 00:42:52 -0400

    layout

--- src/App.js
+++ src/App.js
@@ -14,21 +14,64 @@ class App extends React.Component {
     return (
       <Wrapper darktheme={this.state.darktheme}>
         <ReactTooltip />
-        <div class="row" id="header">
-          <div className="col-md-8">
-            <div className="row">
-              <h1>
-                <span>{this.state.info.name}</span>
-                <span style={{ color: "grey" }}>
-                  {" " + this.state.info.title}
-                </span>
-              </h1>
-            </div>
-            <div className="row">
-              <p>{this.state.info.description}</p>
-            </div>
+
+        <div id="top">
+          <h1>
+            <span>{this.state.info.name}</span>
+            <span style={{ color: "grey" }}>{" " + this.state.info.title}</span>
+          </h1>
+
+          <div id="controls">
+            <img
+              alt="hex view controller"
+              width="32px"
+              height="32px"
+              src={
+                this.state.hexlinkson
+                  ? "./img/icons/list.png"
+                  : "./img/icons/hexagon.png"
+              }
+              data-tip={
+                this.state.hexlinkson
+                  ? "view links as list"
+                  : "view links as hexagons"
+              }
+              onClick={() =>
+                this.setState({ hexlinkson: !this.state.hexlinkson })
+              }
+            />
+            <input
+              type="button"
+              value="Dark"
+              style={{ backgroundColor: "black", color: "white" }}
+              className=""
+              onClick={() =>
+                this.setState({ darktheme: !this.state.darktheme })
+              }
+            />
+            <input
+              type="button"
+              value="Btn 3"
+              style={{ backgroundColor: "black", color: "white" }}
+              className=""
+              onClick={() =>
+                this.setState({ darktheme: !this.state.darktheme })
+              }
+            />
+            <input
+              type="button"
+              value="Btn 4"
+              style={{ backgroundColor: "black", color: "white" }}
+              className=""
+              onClick={() =>
+                this.setState({ darktheme: !this.state.darktheme })
+              }
+            />
           </div>
-          <div className="col-md-3" style={{ "min-height": "300px" }}>
+
+          <p>{this.state.info.description}</p>
+
+          <div id="links">
             {this.state.hexlinkson ? (
               <div>
                 <ul id="projects">
@@ -50,55 +93,23 @@ class App extends React.Component {
               </div>
             )}
           </div>
-          <div className="col-md-1">
-            <div className="row mt-3">
-              <img
-                data-tip="list view"
-                alt="hex view controller"
-                width="32px"
-                height="32px"
-                src={
-                  this.state.hexlinkson
-                    ? "./img/icons/list.png"
-                    : "./img/icons/hexagon.png"
-                }
-                data-tip={
-                  this.state.hexlinkson
-                    ? "view links as list"
-                    : "view links as hexagons"
-                }
-                onClick={() =>
-                  this.setState({ hexlinkson: !this.state.hexlinkson })
-                }
-              />
-            </div>
-            <div className="row my-2">
-              <input
-                type="button"
-                value="Dark"
-                style={{ backgroundColor: "black", color: "white" }}
-                className=""
-                onClick={() =>
-                  this.setState({ darktheme: !this.state.darktheme })
-                }
-              />
-            </div>
-          </div>
         </div>
-        <div class="row" />
-        <ul id="projects" className="clr winkhover">
-          {this.state.projects.map(project => {
-            return (
-              <Hexagon
-                image={project.image}
-                link={project.link}
-                name={project.name}
-                description={project.description}
-                darktheme={this.state.darktheme}
-              />
-            );
-          })}
-        </ul>
+
+        <div id="projects-view">
+          <ul id="projects" className="clr winkhover">
+            {this.state.projects.map(project => {
+              return (
+                <Hexagon
+                  image={project.image}
+                  link={project.link}
+                  name={project.name}
+                  description={project.description}
+                  darktheme={this.state.darktheme}
+                />
+              );
+            })}
+          </ul>
+        </div>
         <div />
       </Wrapper>
     );
